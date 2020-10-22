bd6362192e2d81f26ca6d4594075bc85e94aa82b
Kyran <kyran@gostelow.me>
2020-10-22 15:16:42 +1100

    Timeout working

--- content/hyperdrive/readFile.js
+++ content/hyperdrive/readFile.js
@@ -1,7 +1,11 @@
 // Reference: https://docs.beakerbrowser.com/apis/beaker.hyperdrive#beakerhyperdrivereadfileurl-opts
 function readFile(url, opts = {encoding: 'utf8', timeout: 60000}) {
 	if(typeof opts === 'string') opts = {encoding: opts, timeout: 60000}
-	return fetch(url, {method: 'GET'})
+	const controller = new AbortController()
+	const signal = controller.signal
+	setTimeout(() => controller.abort(), opts.timeout)
+
+	return fetch(url, {method: 'GET', signal: signal})
 		.then(result => {
 			switch(opts.encoding) {
 				case 'json':
@@ -14,6 +18,9 @@ function readFile(url, opts = {encoding: 'utf8', timeout: 60000}) {
 						return text
 					})
 			}
+		})
+		.catch(() => {
+			throw 'Error: Uncaught TimeoutError: Timed out while reading file' // Mimics beaker.hyperdrive.readFile() timeout... not sure if this is good enough though.
 		})
 }
 export default readFile
